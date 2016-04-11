5a5225e5c66c3ce4baece050a6e5d9385674e604
Ramin <ramin_yavari03@yahoo.com>
2016-04-10 21:07:58 -0700

    working on results controller

--- client/app/results/results.js
+++ client/app/results/results.js
@@ -2,8 +2,14 @@ var app = angular.module('mainApp.results',[]);
 
 app.factory('resultsFactory',function($http,$location){
 
-})
+});
 
 app.controller('resultsController',function($scope,$window,$location,resultsFactory){
-  
-})
+  var bar = localStorage.getItem('barInfoObj');
+  JSON.parse(bar);
+
+  $scope.barName = bar.name;
+  $scope.address = bar.address;
+  $scope.phone = bar.phone;
+  $scope.menu = bar.menu;
+});
